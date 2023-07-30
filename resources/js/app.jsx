edc4a58bbcf1fdff89cc0e83770d4b2bf8a05579
“calberth19” <calberth19@gmail.com>
2023-07-30 13:01:43 -0600

    Add Files System

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,10 +2,31 @@ import './bootstrap';
 
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { PrimeReactProvider} from 'primereact/api';
+//Prime components
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+
+//core
+import "primereact/resources/primereact.min.css";
+
+import Main from './Main';
 
 const container = document.getElementById("app");
 const root = createRoot(container);
 
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main />
+    }
+]);
+
 root.render(
-    <div>Hola Mundo!!</div>
+    <PrimeReactProvider>
+        <RouterProvider router={router} />
+    </PrimeReactProvider>
 );
